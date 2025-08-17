116d71393f96974c9057ae03ee23cc2b5484af4d
nguyenvannhan <nguyenvannhan0810@gmail.com>
2025-08-17 09:38:01 +0700

    Remove ziggy generate auto

--- resources/ts/utils/ziggy.ts
+++
@@ -1,5 +0,0 @@
-const Ziggy = { "url": "http:\/\/localhost", "port": null, "defaults": {}, "routes": { "home": { "uri": "\/", "methods": ["GET", "HEAD"] }, "posts.show": { "uri": "posts\/{slug}", "methods": ["GET", "HEAD"], "parameters": ["slug"] }, "login": { "uri": "auth\/login", "methods": ["GET", "HEAD"] }, "login.callback": { "uri": "auth\/callback", "methods": ["GET", "HEAD"] }, "storage.local": { "uri": "storage\/{path}", "methods": ["GET", "HEAD"], "wheres": { "path": ".*" }, "parameters": ["path"] } } };
-if (typeof window !== 'undefined' && typeof window.Ziggy !== 'undefined') {
-  Object.assign(Ziggy.routes, window.Ziggy.routes);
-}
-export { Ziggy };
